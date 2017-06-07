694b6b27533773df9adc99c8dc66059f542e2f75
원정연 <@DESKTOP-7KT6GJJ>
2017-06-07 17:21:04 +0900

    나한테 왜그래!!!

--- OOP_team7/src/ox_Game.java
+++ OOP_team7/src/ox_Game.java
@@ -12,6 +12,11 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextArea;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ox_Game {
 
@@ -77,30 +82,93 @@ public class ox_Game {
 		button_2.setBackground(Color.BLACK);
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
+			gl_panel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addGap(92)
 					.addComponent(button, GroupLayout.PREFERRED_SIZE, 342, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED, 103, Short.MAX_VALUE)
 					.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 342, GroupLayout.PREFERRED_SIZE)
 					.addGap(89))
-				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-					.addContainerGap(38, Short.MAX_VALUE)
-					.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 895, GroupLayout.PREFERRED_SIZE)
-					.addGap(35))
+				.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+					.addGap(34)
+					.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 901, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(33, Short.MAX_VALUE))
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(41)
-					.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 464, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 53, Short.MAX_VALUE)
+					.addGap(31)
+					.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 462, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
 					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 						.addComponent(button, GroupLayout.PREFERRED_SIZE, 334, GroupLayout.PREFERRED_SIZE)
 						.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 334, GroupLayout.PREFERRED_SIZE))
 					.addGap(42))
 		);
 		panel.setLayout(gl_panel);
+		
+		JMenuBar menuBar = new JMenuBar();
+		oxgame.setJMenuBar(menuBar);
+		
+		JMenu mnMenu = new JMenu("Menu");
+		mnMenu.setFont(new Font("Arial", Font.PLAIN, 25));
+		menuBar.add(mnMenu);
+		
+		JMenuItem mntmGoToMenu = new JMenuItem("Go to Menu");
+		mntmGoToMenu.setFont(new Font("Arial", Font.PLAIN, 20));
+		mntmGoToMenu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//menu로 돌아가기
+				MENU home = new MENU();
+				home.frame.setVisible(true);
+				oxgame.setVisible(false);
+			}
+		});
+		mnMenu.add(mntmGoToMenu);
+		
+		JMenu mnQuestion = new JMenu("Question");
+		mnQuestion.setFont(new Font("Arial", Font.PLAIN, 25));
+		menuBar.add(mnQuestion);
+		
+		JMenuItem mntmNo = new JMenuItem("No.1");
+		mntmNo.setFont(new Font("Arial", Font.PLAIN, 20));
+		mnQuestion.add(mntmNo);
+		
+		JMenuItem mntmNo_1 = new JMenuItem("No.2");
+		mntmNo_1.setFont(new Font("Arial", Font.PLAIN, 20));
+		mnQuestion.add(mntmNo_1);
+		
+		JMenuItem mntmNo_2 = new JMenuItem("No.3");
+		mntmNo_2.setFont(new Font("Arial", Font.PLAIN, 20));
+		mnQuestion.add(mntmNo_2);
+		
+		JMenuItem mntmNo_3 = new JMenuItem("No.4");
+		mntmNo_3.setFont(new Font("Arial", Font.PLAIN, 20));
+		mnQuestion.add(mntmNo_3);
+		
+		JMenuItem mntmNo_4 = new JMenuItem("No.5");
+		mntmNo_4.setFont(new Font("Arial", Font.PLAIN, 20));
+		mnQuestion.add(mntmNo_4);
+		
+		JMenuItem mntmNo_5 = new JMenuItem("No.6");
+		mntmNo_5.setFont(new Font("Arial", Font.PLAIN, 20));
+		mnQuestion.add(mntmNo_5);
+		
+		JMenuItem mntmNo_6 = new JMenuItem("No.7");
+		mntmNo_6.setFont(new Font("Arial", Font.PLAIN, 20));
+		mnQuestion.add(mntmNo_6);
+		
+		JMenuItem mntmNo_7 = new JMenuItem("No.8");
+		mntmNo_7.setFont(new Font("Arial", Font.PLAIN, 20));
+		mnQuestion.add(mntmNo_7);
+		
+		JMenuItem mntmNo_8 = new JMenuItem("No.9");
+		mntmNo_8.setFont(new Font("Arial", Font.PLAIN, 20));
+		mnQuestion.add(mntmNo_8);
+		
+		JMenuItem mntmNo_9 = new JMenuItem("No.10");
+		mntmNo_9.setFont(new Font("Arial", Font.PLAIN, 20));
+		mnQuestion.add(mntmNo_9);
 		
 		/*ox_question question = new ox_question();
 		textArea.setText(question.ox_quiz.get(counter));*/
