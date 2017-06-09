7e921045190fc85d84e69b151da200bed158fe0f
Eumjihye <jihye97@ajou.ac.kr>
2017-06-09 20:27:58 +0900

    글씨

--- OOP_team7/src/Blank_Game.java
+++ OOP_team7/src/Blank_Game.java
@@ -148,6 +148,7 @@ public class Blank_Game {
 		textField.setColumns(10);
 
 		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.setBackground(new Color(255, 255, 255));
 		btnSubmit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Blank_solution bs = new Blank_solution();
@@ -225,11 +226,11 @@ public class Blank_Game {
 		frame.setJMenuBar(menuBar);
 
 		JMenu mnMenu = new JMenu("Menu");
-		mnMenu.setFont(new Font("Arial", Font.PLAIN, 25));
+		mnMenu.setFont(new Font("Arial", Font.PLAIN, 30));
 		menuBar.add(mnMenu);
 
 		JMenuItem mntmGoToMenu = new JMenuItem("Go to Menu");
-		mntmGoToMenu.setFont(new Font("Arial", Font.PLAIN, 20));
+		mntmGoToMenu.setFont(new Font("Arial", Font.PLAIN, 25));
 		mntmGoToMenu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				bq.setnum(0);
@@ -242,7 +243,7 @@ public class Blank_Game {
 		mnMenu.add(mntmGoToMenu);
 
 		JMenu mnQuestion = new JMenu("Question");
-		mnQuestion.setFont(new Font("Arial", Font.PLAIN, 25));
+		mnQuestion.setFont(new Font("Arial", Font.PLAIN, 30));
 		menuBar.add(mnQuestion);
 
 		JMenuItem mntmNo = new JMenuItem("No.1");
@@ -255,7 +256,7 @@ public class Blank_Game {
 				bg.frame.setVisible(false);
 			}
 		});
-		mntmNo.setFont(new Font("Arial", Font.PLAIN, 20));
+		mntmNo.setFont(new Font("Arial", Font.PLAIN, 25));
 		mnQuestion.add(mntmNo);
 
 		JMenuItem mntmNo_1 = new JMenuItem("No.2");
@@ -268,7 +269,7 @@ public class Blank_Game {
 				bg.frame.setVisible(false);
 			}
 		});
-		mntmNo_1.setFont(new Font("Arial", Font.PLAIN, 20));
+		mntmNo_1.setFont(new Font("Arial", Font.PLAIN, 25));
 		mnQuestion.add(mntmNo_1);
 
 		JMenuItem mntmNo_2 = new JMenuItem("No.3");
@@ -280,7 +281,7 @@ public class Blank_Game {
 				bg.frame.setVisible(false);
 			}
 		});
-		mntmNo_2.setFont(new Font("Arial", Font.PLAIN, 20));
+		mntmNo_2.setFont(new Font("Arial", Font.PLAIN, 25));
 		mnQuestion.add(mntmNo_2);
 
 		JMenuItem mntmNo_3 = new JMenuItem("No.4");
@@ -292,7 +293,7 @@ public class Blank_Game {
 				bg.frame.setVisible(false);
 			}
 		});
-		mntmNo_3.setFont(new Font("Arial", Font.PLAIN, 20));
+		mntmNo_3.setFont(new Font("Arial", Font.PLAIN, 25));
 		mnQuestion.add(mntmNo_3);
 
 		JMenuItem mntmNo_4 = new JMenuItem("No.5");
@@ -304,7 +305,7 @@ public class Blank_Game {
 				bg.frame.setVisible(false);
 			}
 		});
-		mntmNo_4.setFont(new Font("Arial", Font.PLAIN, 20));
+		mntmNo_4.setFont(new Font("Arial", Font.PLAIN, 25));
 		mnQuestion.add(mntmNo_4);
 
 		JMenuItem mntmNo_5 = new JMenuItem("No.6");
@@ -316,7 +317,7 @@ public class Blank_Game {
 				bg.frame.setVisible(false);
 			}
 		});
-		mntmNo_5.setFont(new Font("Arial", Font.PLAIN, 20));
+		mntmNo_5.setFont(new Font("Arial", Font.PLAIN, 25));
 		mnQuestion.add(mntmNo_5);
 
 		JMenuItem mntmNo_6 = new JMenuItem("No.7");
@@ -328,7 +329,7 @@ public class Blank_Game {
 				bg.frame.setVisible(false);
 			}
 		});
-		mntmNo_6.setFont(new Font("Arial", Font.PLAIN, 20));
+		mntmNo_6.setFont(new Font("Arial", Font.PLAIN, 25));
 		mnQuestion.add(mntmNo_6);
 
 		JMenuItem mntmNo_7 = new JMenuItem("No.8");
@@ -340,7 +341,7 @@ public class Blank_Game {
 				bg.frame.setVisible(false);
 			}
 		});
-		mntmNo_7.setFont(new Font("Arial", Font.PLAIN, 20));
+		mntmNo_7.setFont(new Font("Arial", Font.PLAIN, 25));
 		mnQuestion.add(mntmNo_7);
 
 		JMenuItem mntmNo_8 = new JMenuItem("No.9");
@@ -352,7 +353,7 @@ public class Blank_Game {
 				bg.frame.setVisible(false);
 			}
 		});
-		mntmNo_8.setFont(new Font("Arial", Font.PLAIN, 20));
+		mntmNo_8.setFont(new Font("Arial", Font.PLAIN, 25));
 		mnQuestion.add(mntmNo_8);
 
 		JMenuItem mntmNo_9 = new JMenuItem("No.10");
@@ -364,7 +365,7 @@ public class Blank_Game {
 				bg.frame.setVisible(false);
 			}
 		});
-		mntmNo_9.setFont(new Font("Arial", Font.PLAIN, 20));
+		mntmNo_9.setFont(new Font("Arial", Font.PLAIN, 25));
 		mnQuestion.add(mntmNo_9);
 
 	}
