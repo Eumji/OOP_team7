2d5e05de52a3e7b55a34ab3cedbec712c09b402d
Eumjihye <jihye97@ajou.ac.kr>
2017-06-09 13:46:13 +0900

    지혜 김치찌개먹구옴

--- OOP_team7/src/Blank_question.java
+++ OOP_team7/src/Blank_question.java
@@ -12,94 +12,93 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 public class Blank_question {
-	public ArrayList<Icon> question(){
-		ArrayList<Icon> img = new ArrayList<>();
+   public ArrayList<Icon> question(){
+      ArrayList<Icon> img = new ArrayList<>();
 
-		/*
-		Icon i1 = (Icon) new ImageIcon(this.getClass().getResource("No.1.jpg")).getImage();
-		Icon i2 = (Icon) new ImageIcon(this.getClass().getResource("No.2.jpg")).getImage();
-		Icon i3 = (Icon) new ImageIcon(this.getClass().getResource("No.3.jpg")).getImage();
-		Icon i4 = (Icon) new ImageIcon(this.getClass().getResource("No.4.jpg")).getImage();
-		
-		img.add(i1);
-		img.add(i2);
-		img.add(i3);
-		img.add(i4);
-		
-		*/
-		return img;
-	}
-	
-	public ArrayList<String> answer(){
-		ArrayList<String> blank_answer;
-		BufferedReader br = null;
-		blank_answer = new ArrayList<String>();
-		try{
-			br = new BufferedReader(new FileReader("blank_answer.txt"));
-			String line;
-			
-			while((line = br.readLine())!=null){
-				blank_answer.add(line);
-			}
-			
-		br.close();
-		}catch(FileNotFoundException fnfe){
-			System.out.println("File is not found.");
-		}catch(IOException ioe){
-			System.out.println("File input//output error.");
-		}
-		return blank_answer;
-	}
-	
-	public ArrayList<String> solution(){
-		ArrayList<String> blank_solution;
-		BufferedReader br = null;
-		blank_solution = new ArrayList<String>();
-		try{
-			br = new BufferedReader(new FileReader("blank_solution.txt"));
-			String line;
-			
-			while((line = br.readLine())!=null){
-				blank_solution.add(line);
-			}
-			
-		br.close();
-		}catch(FileNotFoundException fnfe){
-			System.out.println("File is not found.");
-		}catch(IOException ioe){
-			System.out.println("File input//output error.");
-		}
-		return blank_solution;
-	}
-	
-	public int getnum(){
-		int number=-1;
-		BufferedReader br = null;
-		try{
-			br = new BufferedReader(new FileReader("blank_number.txt"));
-			String line=br.readLine();
-			number = Integer.parseInt(line);	
-		br.close();
-		}catch(FileNotFoundException fnfe){
-			System.out.println("File is not found.");
-		}catch(IOException ioe){
-			System.out.println("File input//output error.");
-		}
-		return number;
-	}
-	
-	public void setnum(int n){
-		BufferedWriter bw = null;
-		String line = null;
-		line = Integer.toString(n);
-		try {
-			bw = new BufferedWriter(new FileWriter("blank_counter.txt"));
-			bw.write(line);
-			bw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+      /*
+      Icon i1 = (Icon) new ImageIcon(this.getClass().getResource("No.1.jpg")).getImage();
+      Icon i2 = (Icon) new ImageIcon(this.getClass().getResource("No.2.jpg")).getImage();
+      Icon i3 = (Icon) new ImageIcon(this.getClass().getResource("No.3.jpg")).getImage();
+      Icon i4 = (Icon) new ImageIcon(this.getClass().getResource("No.4.jpg")).getImage();
+      
+      img.add(i1);
+      img.add(i2);
+      img.add(i3);
+      img.add(i4);
+      
+      */
+      return img;
+   }
+   
+   public ArrayList<String> answer(){
+      ArrayList<String> blank_answer;
+      BufferedReader br = null;
+      blank_answer = new ArrayList<String>();
+      try{
+         br = new BufferedReader(new FileReader("blank_answer.txt"));
+         String line;
+         while((line = br.readLine())!=null){
+            blank_answer.add(line);
+         }
+         
+      br.close();
+      }catch(FileNotFoundException fnfe){
+         System.out.println("File is not found.");
+      }catch(IOException ioe){
+         System.out.println("File input//output error.");
+      }
+      return blank_answer;
+   }
+   
+   public ArrayList<String> solution(){
+      ArrayList<String> blank_solution;
+      BufferedReader br = null;
+      blank_solution = new ArrayList<String>();
+      try{
+         br = new BufferedReader(new FileReader("blank_solution.txt"));
+         String line;
+         
+         while((line = br.readLine())!=null){
+            blank_solution.add(line);
+         }
+         
+      br.close();
+      }catch(FileNotFoundException fnfe){
+         System.out.println("File is not found.");
+      }catch(IOException ioe){
+         System.out.println("File input//output error.");
+      }
+      return blank_solution;
+   }
+   
+   public int getnum(){
+      int number=-1;
+      BufferedReader br = null;
+      try{
+         br = new BufferedReader(new FileReader("blank_number.txt"));
+         String line=br.readLine();
+         number = Integer.parseInt(line);   
+      br.close();
+      }catch(FileNotFoundException fnfe){
+         System.out.println("File is not found.");
+      }catch(IOException ioe){
+         System.out.println("File input//output error.");
+      }
+      return number;
+   }
+   
+   public void setnum(int n){
+      BufferedWriter bw = null;
+      String line = null;
+      line = Integer.toString(n);
+      try {
+         bw = new BufferedWriter(new FileWriter("blank_number.txt"));
+         bw.write(line);
+         bw.close();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
 
-	
+   
 }
--- OOP_team7/src/Blank_solution.java
+++ OOP_team7/src/Blank_solution.java
@@ -19,205 +19,204 @@ import javax.swing.JButton;
 
 public class Blank_solution {
 
-	
-	boolean Blank_isCorrect;
-	private Blank_question bq = new Blank_question();
-	Blank_Game bg = new Blank_Game();
-	
-	
-	JFrame frame;
-	JTextArea textArea;
-	private JMenuBar menuBar;
-	JLabel lblNewLabel;
-	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Blank_solution window = new Blank_solution();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+   
+   boolean Blank_isCorrect;
+   private Blank_question bq = new Blank_question();
+   Blank_Game bg = new Blank_Game();
+   
+   
+   JFrame frame;
+   JTextArea textArea;
+   private JMenuBar menuBar;
+   JLabel lblNewLabel;
+   
+   /**
+    * Launch the application.
+    */
+   public static void main(String[] args) {
+      EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            try {
+               Blank_solution window = new Blank_solution();
+               window.frame.setVisible(true);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+         }
+      });
+   }
 
-	/**
-	 * Create the application.
-	 */
-	public Blank_solution() {
-		initialize();
-	}
+   /**
+    * Create the application.
+    */
+   public Blank_solution() {
+      initialize();
+   }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 1000, 1000);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		textArea = new JTextArea();
-		textArea.setForeground(Color.BLACK);
-		textArea.setOpaque(false);
-		JButton btnNewButton = new JButton("Next");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				bq.setnum(bq.getnum()+1);
-				bg.frame.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		btnNewButton.setFont(new Font("Arial", Font.BOLD, 30));
-		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setBackground(Color.BLACK);
-		btnNewButton.setBounds(829, 845, 132, 53);
-		frame.getContentPane().add(btnNewButton);
-		
-		lblNewLabel = new JLabel("");
-		lblNewLabel.setBounds(0, 0, 978, 913);
-		frame.getContentPane().add(lblNewLabel);
-		
-		menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
-		
-		JMenu mnMenu = new JMenu("Menu");
-		mnMenu.setFont(new Font("Arial", Font.PLAIN, 25));
-		menuBar.add(mnMenu);
-		
-		JMenuItem mntmGoToMenu = new JMenuItem("Go to Menu");
-		mntmGoToMenu.setFont(new Font("Arial", Font.PLAIN, 20));
-		mntmGoToMenu.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				//menu로 돌아가기
-				MENU home = new MENU();
-				home.frame.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		mnMenu.add(mntmGoToMenu);
-		
-		JMenu mnQuestion = new JMenu("Question");
-		mnQuestion.setFont(new Font("Arial", Font.PLAIN, 25));
-		menuBar.add(mnQuestion);
-		
+   /**
+    * Initialize the contents of the frame.
+    */
+   private void initialize() {
+      frame = new JFrame();
+      frame.setBounds(100, 100, 1000, 1000);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.getContentPane().setLayout(null);
+      
+      textArea = new JTextArea();
+      textArea.setForeground(Color.BLACK);
+      textArea.setOpaque(false);
+      JButton btnNewButton = new JButton("Next");
+      btnNewButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent arg0) { 
+        	bq.setnum(bq.getnum()+1);
+            bg.frame.setVisible(true);
+            frame.setVisible(false);
+         }
+      });
+      btnNewButton.setFont(new Font("Arial", Font.BOLD, 30));
+      btnNewButton.setForeground(Color.WHITE);
+      btnNewButton.setBackground(Color.BLACK);
+      btnNewButton.setBounds(829, 845, 132, 53);
+      frame.getContentPane().add(btnNewButton);
+      
+      lblNewLabel = new JLabel("");
+      lblNewLabel.setBounds(0, 0, 978, 913);
+      frame.getContentPane().add(lblNewLabel);
+      
+      menuBar = new JMenuBar();
+      frame.setJMenuBar(menuBar);
+      
+      JMenu mnMenu = new JMenu("Menu");
+      mnMenu.setFont(new Font("Arial", Font.PLAIN, 25));
+      menuBar.add(mnMenu);
+      
+      JMenuItem mntmGoToMenu = new JMenuItem("Go to Menu");
+      mntmGoToMenu.setFont(new Font("Arial", Font.PLAIN, 20));
+      mntmGoToMenu.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent arg0) {
+            //menu로 돌아가기
+            MENU home = new MENU();
+            home.frame.setVisible(true);
+            frame.setVisible(false);
+         }
+      });
+      mnMenu.add(mntmGoToMenu);
+      
+      JMenu mnQuestion = new JMenu("Question");
+      mnQuestion.setFont(new Font("Arial", Font.PLAIN, 25));
+      menuBar.add(mnQuestion);
+      
 
-		JMenuItem mntmNo = new JMenuItem("No.1");
-		mntmNo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				bq.setnum(0);
-				bg.frame.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		mntmNo.setFont(new Font("Arial", Font.PLAIN, 20));
-		mnQuestion.add(mntmNo);
-		
-		JMenuItem mntmNo_1 = new JMenuItem("No.2");
-		mntmNo_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				bq.setnum(1);
-				bg.frame.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		mntmNo_1.setFont(new Font("Arial", Font.PLAIN, 20));
-		mnQuestion.add(mntmNo_1);
-		
-		JMenuItem mntmNo_2 = new JMenuItem("No.3");
-		mntmNo_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				bq.setnum(2);
-				bg.frame.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		mntmNo_2.setFont(new Font("Arial", Font.PLAIN, 20));
-		mnQuestion.add(mntmNo_2);
-		
-		JMenuItem mntmNo_3 = new JMenuItem("No.4");
-		mntmNo_3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				bq.setnum(3);
-				bg.frame.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		mntmNo_3.setFont(new Font("Arial", Font.PLAIN, 20));
-		mnQuestion.add(mntmNo_3);
-		
-		JMenuItem mntmNo_4 = new JMenuItem("No.5");
-		mntmNo_4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				bq.setnum(4);
-				bg.frame.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		mntmNo_4.setFont(new Font("Arial", Font.PLAIN, 20));
-		mnQuestion.add(mntmNo_4);
-		
-		JMenuItem mntmNo_5 = new JMenuItem("No.6");
-		mntmNo_5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				bq.setnum(5);
-				bg.frame.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		mntmNo_5.setFont(new Font("Arial", Font.PLAIN, 20));
-		mnQuestion.add(mntmNo_5);
-		
-		JMenuItem mntmNo_6 = new JMenuItem("No.7");
-		mntmNo_6.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				bq.setnum(6);
-				bg.frame.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		mntmNo_6.setFont(new Font("Arial", Font.PLAIN, 20));
-		mnQuestion.add(mntmNo_6);
-		
-		JMenuItem mntmNo_7 = new JMenuItem("No.8");
-		mntmNo_7.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				bq.setnum(7);
-				bg.frame.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		mntmNo_7.setFont(new Font("Arial", Font.PLAIN, 20));
-		mnQuestion.add(mntmNo_7);
-		
-		JMenuItem mntmNo_8 = new JMenuItem("No.9");
-		mntmNo_8.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				bq.setnum(8);
-				bg.frame.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		mntmNo_8.setFont(new Font("Arial", Font.PLAIN, 20));
-		mnQuestion.add(mntmNo_8);
-		
-		JMenuItem mntmNo_9 = new JMenuItem("No.10");
-		mntmNo_9.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				bq.setnum(9);
-				bg.frame.setVisible(true);
-				frame.setVisible(false);
-			}
-		});
-		mntmNo_9.setFont(new Font("Arial", Font.PLAIN, 20));
-		mnQuestion.add(mntmNo_9);
-		
-	}
-	
+      JMenuItem mntmNo = new JMenuItem("No.1");
+      mntmNo.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent arg0) {
+            bq.setnum(0);
+            bg.frame.setVisible(true);
+            frame.setVisible(false);
+         }
+      });
+      mntmNo.setFont(new Font("Arial", Font.PLAIN, 20));
+      mnQuestion.add(mntmNo);
+      
+      JMenuItem mntmNo_1 = new JMenuItem("No.2");
+      mntmNo_1.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            bq.setnum(1);
+            bg.frame.setVisible(true);
+            frame.setVisible(false);
+         }
+      });
+      mntmNo_1.setFont(new Font("Arial", Font.PLAIN, 20));
+      mnQuestion.add(mntmNo_1);
+      
+      JMenuItem mntmNo_2 = new JMenuItem("No.3");
+      mntmNo_2.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            bq.setnum(2);
+            bg.frame.setVisible(true);
+            frame.setVisible(false);
+         }
+      });
+      mntmNo_2.setFont(new Font("Arial", Font.PLAIN, 20));
+      mnQuestion.add(mntmNo_2);
+      
+      JMenuItem mntmNo_3 = new JMenuItem("No.4");
+      mntmNo_3.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            bq.setnum(3);
+            bg.frame.setVisible(true);
+            frame.setVisible(false);
+         }
+      });
+      mntmNo_3.setFont(new Font("Arial", Font.PLAIN, 20));
+      mnQuestion.add(mntmNo_3);
+      
+      JMenuItem mntmNo_4 = new JMenuItem("No.5");
+      mntmNo_4.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            bq.setnum(4);
+            bg.frame.setVisible(true);
+            frame.setVisible(false);
+         }
+      });
+      mntmNo_4.setFont(new Font("Arial", Font.PLAIN, 20));
+      mnQuestion.add(mntmNo_4);
+      
+      JMenuItem mntmNo_5 = new JMenuItem("No.6");
+      mntmNo_5.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            bq.setnum(5);
+            bg.frame.setVisible(true);
+            frame.setVisible(false);
+         }
+      });
+      mntmNo_5.setFont(new Font("Arial", Font.PLAIN, 20));
+      mnQuestion.add(mntmNo_5);
+      
+      JMenuItem mntmNo_6 = new JMenuItem("No.7");
+      mntmNo_6.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            bq.setnum(6);
+            bg.frame.setVisible(true);
+            frame.setVisible(false);
+         }
+      });
+      mntmNo_6.setFont(new Font("Arial", Font.PLAIN, 20));
+      mnQuestion.add(mntmNo_6);
+      
+      JMenuItem mntmNo_7 = new JMenuItem("No.8");
+      mntmNo_7.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            bq.setnum(7);
+            bg.frame.setVisible(true);
+            frame.setVisible(false);
+         }
+      });
+      mntmNo_7.setFont(new Font("Arial", Font.PLAIN, 20));
+      mnQuestion.add(mntmNo_7);
+      
+      JMenuItem mntmNo_8 = new JMenuItem("No.9");
+      mntmNo_8.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            bq.setnum(8);
+            bg.frame.setVisible(true);
+            frame.setVisible(false);
+         }
+      });
+      mntmNo_8.setFont(new Font("Arial", Font.PLAIN, 20));
+      mnQuestion.add(mntmNo_8);
+      
+      JMenuItem mntmNo_9 = new JMenuItem("No.10");
+      mntmNo_9.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            bq.setnum(9);
+            bg.frame.setVisible(true);
+            frame.setVisible(false);
+         }
+      });
+      mntmNo_9.setFont(new Font("Arial", Font.PLAIN, 20));
+      mnQuestion.add(mntmNo_9);
+      
+   }
+   
 }
