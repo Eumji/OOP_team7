ea8003ea0782b9dbc89bb67a756ca26991b18bb7
sprin <sprin@DESKTOP-SKJ350I>
2017-06-12 11:15:36 +0900

    ss

--- OOP_team7/src/IsHas.java
+++ OOP_team7/src/IsHas.java
@@ -3,7 +3,24 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import java.awt.Font;
 
 public class IsHas extends JFrame {
 
@@ -30,11 +47,107 @@ public class IsHas extends JFrame {
 	 */
 	public IsHas() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1000, 1000);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("MENU");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Go to Menu");
+		mntmNewMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				MENU M = new MENU();
+				M.frame.setVisible(true);
+				setVisible(false);
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Basic Menu");
+		mntmNewMenuItem_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				BasicMenu bm = new BasicMenu();
+				bm.setVisible(true);
+				setVisible(false);
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Encapsulation Menu");
+		mntmNewMenuItem_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EncapsulationMenu em = new EncapsulationMenu();
+				em.setVisible(true);
+				setVisible(false);
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_2);
+		
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Inheritance Menu");
+		mntmNewMenuItem_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				InheritanceMenu im = new InheritanceMenu();
+				im.setVisible(true);
+				setVisible(false);
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_3);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(240, 248, 255));
+		panel.setBounds(89, 391, 795, 246);
+		contentPane.add(panel);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setFont(new Font("Calibri Light", Font.PLAIN, 20));
+		textArea.setBackground(new Color(240, 248, 255));
+		
+		File ihF = new File("ishas.txt");
 
+		FileReader ihfr;
+		try {
+			ihfr = new FileReader(ihF);
+			BufferedReader br = new BufferedReader(ihfr);
+
+			String line;
+
+			do {
+				line = br.readLine();
+				textArea.append(line);
+				textArea.append("\n");
+				
+			} while (line != null);
+
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		textArea.setCaretPosition(0);
+		
+		JScrollPane scroll = new JScrollPane(textArea);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addComponent(scroll, GroupLayout.DEFAULT_SIZE, 795, Short.MAX_VALUE)
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addComponent(scroll, GroupLayout.DEFAULT_SIZE, 299, Short.MAX_VALUE)
+		);
+		panel.setLayout(gl_panel);
+	}
 }
