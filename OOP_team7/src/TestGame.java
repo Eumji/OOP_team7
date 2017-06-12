95ae84d268cde7c4dac6c391250cec94edb75036
원정연 <@DESKTOP-7KT6GJJ>
2017-06-12 11:31:06 +0900

    느낌표를 없애자!

--- OOP_team7/src/TestGame.java
+++ OOP_team7/src/TestGame.java
@@ -1,14 +1,12 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.ImageIcon;
 import java.awt.Color;
 
 public class TestGame {
