72720cf25252e0ca78520ffd3932b557f94a7ae6
LGPC <LGPC@DESKTOP-ADURSBS>
2017-06-12 11:21:53 +0900

    a

--- OOP_team7/src/MultipleChoice_Game.java
+++ OOP_team7/src/MultipleChoice_Game.java
@@ -21,7 +21,7 @@ import java.awt.event.ActionEvent;
 import java.awt.SystemColor;
 import javax.swing.Icon;
 
-public class MultipleChoice_Game {
+public class MultipleChoice_Game extends TestGame{
 
 	JFrame frame;
 	JTextArea textArea;
